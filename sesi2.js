71439e3a5c186a24c16178374ddd7c00b67b8313
Paskal00040 <146911234+Paskal00040@users.noreply.github.com>
2023-10-28 02:17:50 +0700

    Create sesi2.js

---
+++ sesi2.js
@@ -0,0 +1,9 @@
+const firstName = "Lince Paskalina";
+const lastName = "Kegiye";
+const age = 20;
+const isMarried = false; //
+
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + (isMarried ? "Menikah" : "Belum Menikah"));
